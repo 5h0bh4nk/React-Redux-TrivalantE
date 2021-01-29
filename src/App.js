af3d758291be09e455a2a39bb3aef76ebb7734d2
Shubh_ank <62099471+5h0bh4nk@users.noreply.github.com>
2021-01-29 18:42:29 +0530

    spa 1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React from 'react';
-import ParticlesBg from 'particles-bg'
 import Main from './components/MainComponent'
 import { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 
-let config = {
-  num: [4, 7],
-  rps: 0.1,
-  radius: [5, 10],
-  life: [1.5, 3],
-  v: [2, 3],
-  tha: [-40, 40],
-  // body: "./img/icon.png", // Whether to render pictures
-  alpha: [0.6, 0],
-  scale: [1, 0.1],
-  position: "all", // all or center or {x:1,y:1,width:100,height:100}
-  color: ["random", "#ff0000"],
-  bround: "dead", // cross or bround
-  random: null,  // or null,
-  g: 5,    // gravity
-  // f: [2, -1], // force
-  onParticleUpdate: (ctx, particle) => {
-      ctx.beginPath();
-      ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
-      ctx.fillStyle = particle.color;
-      ctx.fill();
-      ctx.closePath();
-  }
-};
 
 
 class App extends Component{
@@ -36,10 +11,7 @@ render(){
   return (
     <>
     <BrowserRouter>
-    <div>
       <Main />
-    </div>
-    <ParticlesBg type="random" num={10} bg={true} />
     </BrowserRouter>
     </>
   );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,36 +7,49 @@ import '../App.css';
 import {DISHES} from '../shared/dishes'
 import { Switch , Route , Redirect } from 'react-router-dom';
 import { Component } from 'react';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishDetailComponent';
-
+import Contact from './ContactComponent';
 
 
 class Main extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-      dishes : DISHES,
-      selectedDish: null
-    };
-  }
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+      };
+    }
 
   onDishSelect(dishId){
     this.setState({ selectedDish : dishId});
   }
 
 render(){
-  const HomePage = () =>{
+
+  const HomePage = () => {
     return(
-      <Home />
+        <Home 
+            dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+            leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
     );
   }
+  
   return (
     <div>
       <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path = "/menu" component = {() => <Menu dishes = {this.state.dishes} />}/>
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to ="/home" />
         </Switch>
       <Footer />
