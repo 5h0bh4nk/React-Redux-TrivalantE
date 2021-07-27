ec54721e3679f66c1d0bcd2ea63f11a4b21e7817
Shubh4nk <kulshr_971981@student.nitw.ac.in>
2021-07-27 12:47:31 +0530

    Update FooterComponent.js

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -17,12 +17,12 @@ function Footer(props) {
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-		              <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+		              123 , tajganj<br />
+		              Agra, U.P<br />
+		              INDIA<br />
+		              <i className="fa fa-phone fa-lg"></i>: +91 1234 5678<br />
+		              <i className="fa fa-fax fa-lg"></i>: +91 8765 4321<br />
+		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:kul.shubhank207@gmail.com">
                          confusion@food.net</a>
                     </address>
                 </div>
@@ -39,7 +39,7 @@ function Footer(props) {
             </div>
             <div className="row justify-content-center">             
                 <div className="col-auto">
-                    <p>© Copyright 2018 Ristorante Con Fusion</p>
+                    <p>© Copyright 2020 Ristorante Con Trivalante</p>
                 </div>
             </div>
         </div>
